3decbf56b19a73b9fe3103f2c251bcd69de92214
Christian <cjlaborde@gmail.com>
2020-09-13 20:20:36 -0400

    Group Transitions

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Modal</router-link>|
-      <router-link to="/about">About</router-link>
+      <router-link to="/list">List</router-link>
     </div>
-    <!-- <transition name="fade" mode="out-in"> -->
     <transition name="slide-fade" mode="out-in">
       <router-view />
     </transition>
@@ -21,16 +20,31 @@
 }
 
 button {
-  width: 5em;
-  height: 2em;
+  height: 2.2em;
   margin: 0.5em;
-  border-radius: 5px;
+  border-radius: 0.3em;
   background: linear-gradient(to right, #16c0b0, #84cf6a);
-  font-size: 1em;
   color: white;
-  border: none;
-  outline: none;
-  cursor: pointer;
+}
+
+input {
+  display: inline-block;
+  box-sizing: border-box;
+  width: 30%;
+  height: 1.8em;
+  padding: 0.5em;
+  margin: 0.2em;
+  font: 0.8em 'Avenir', Helvetica, sans-serif;
+}
+
+.modal {
+  width: 250px;
+  height: 200px;
+  padding: 10px;
+  margin: 0 auto;
+  margin-top: 20px;
+  border-radius: 2%;
+  background-color: #e0e0e0;
 }
 
 #nav {
@@ -46,24 +60,31 @@ button {
   color: #42b983;
 }
 
-.modal {
-  width: 250px;
-  height: 200px;
-  padding: 10px;
-  margin: 0 auto;
-  margin-top: 20px;
-  border-radius: 2%;
-  background-color: #e0e0e0;
+p {
+  font-size: 0.8em;
 }
+
+::placeholder {
+  color: #2c3e50;
+}
+
+ul {
+  list-style-type: none;
+}
+
 /*** TRANSITIONS ***/
-.fade-enter,
-.fade-leave-to {
+/*** TRANSITIONS ***/
+.fade-enter {
   opacity: 0;
 }
 
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s ease-out;
+}
+
+.fade-leave-to {
+  opacity: 0;
 }
 
 .slide-fade-enter {
@@ -79,5 +100,18 @@ button {
 .slide-fade-leave-to {
   transform: translateX(-10px);
   opacity: 0;
+}
+
+.slide-up-enter {
+  transform: translateY(10px);
+  opacity: 0;
+}
+
+.slide-up-enter-active {
+  transition: all 0.2s ease;
+}
+
+.slide-up-move {
+  transition: transform 0.8s ease-in;
 }
 </style>
